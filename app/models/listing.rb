cbcd8c03b7250d1b659df351acdd5aeab49cc76c
Ghita Dahoune <ghita.dahoune96@gmail.com>
2024-01-23 14:48:16 +0100

    Added foreign keys, validation and routes

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,6 @@
 class Listing < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :user_id, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_23_115224) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "listings", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.string "image_url"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.string "message"
+    t.bigint "user_id"
+    t.bigint "listing_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["listing_id"], name: "index_requests_on_listing_id"
+    t.index ["user_id"], name: "index_requests_on_user_id"
+  end
 
 end

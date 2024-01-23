d6bb3dcf91c219a3463182d31b9c6473e2349590
AgentSmithScript <ilyass.supemir@gmail.com>
2024-01-23 15:30:04 +0100

    added permits to requests controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -20,4 +20,8 @@ class RequestsController < ApplicationController
   def set_request
     @request = Request.find(params[:id])
   end
+
+  def request_params
+    params.require(:request).permit(:message)
+  end
 end

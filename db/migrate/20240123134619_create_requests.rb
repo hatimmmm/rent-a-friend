1d0dbf2281cdcc5233c51ce2a1e7ef8be4a7e25b
hatimmmm <78708038+hatimmmm@users.noreply.github.com>
2024-01-24 13:41:09 +0100

    fixed request migration/schema, seeds, listing and some other suff (#7)

--- db/migrate/20240123134619_create_requests.rb
+++ db/migrate/20240123134619_create_requests.rb
@@ -2,7 +2,8 @@ class CreateRequests < ActiveRecord::Migration[7.1]
   def change
     create_table :requests do |t|
       t.string :message
-      t.references :user, :listing , null: false, foreign_key: true
+      t.references :user , null: false, foreign_key: true
+      t.references :listing , null: false, foreign_key: true
 
       t.timestamps
     end
